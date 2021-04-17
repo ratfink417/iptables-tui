1c5ea7acd77c7b11654f03119b46f5d767aeb03d
ratfink417 <ratfink417@gmail.com>
2021-04-16 22:42:05 -0700

    reduced code by a lot

--- iptab.py
+++ iptab.py
@@ -1,72 +1,35 @@
 #!/usr/bin/env python3
 import npyscreen
+import iptc
 
-### BEGIN MAIN FORM
+# chains = iptc.easy.dump_table('filter', ipv6=False)
+# for key in chains:
+#     print(key)
+# rules  = iptc.easy.dump_chain('filter', 'OUTPUT', ipv6=False)
+
+## BEGIN MAIN FORM
 class TableList(npyscreen.MultiLineAction):
     def __init__(self, *args, **keywords):
         super(TableList, self).__init__(*args, **keywords)
-        self.values = ['FILTER','NAT','MANGLE','RAW','SELINUX']
+        self.values = ['filter','nat','mangle','raw','selinux']
 
     def actionHighlighted(self, act_on_this, key_press):
         selected_table = self.values[self.cursor_line]
-        if   selected_table == "FILTER" :  self.parent.parentApp.switchForm("FILTER")
-        elif selected_table == "NAT"    :  self.parent.parentApp.switchForm("NAT")
-        elif selected_table == "MANGLE" :  self.parent.parentApp.switchForm("MANGLE")
-        elif selected_table == "RAW"    :  self.parent.parentApp.switchForm("RAW")
-        elif selected_table == "SELINUX":  self.parent.parentApp.switchForm("SELINUX")
-        else                            :  self.parent.parentApp.switchForm(None)
- 
+        self.values=[]
+        chains = iptc.easy.dump_table(selected_table, ipv6=False)
+
+        for key in chains:
+            self.values.append(key)
+
+        self.display()
+
 class table_Select_Form(npyscreen.Form):
     def create(self):
-        self.Table_Choice = self.add(TableList, max_height = 6, name = "Select a table", scroll_exit = True)
-
-### END MAIN FORM
-#################################################
-## BEGIN CHAIN SELECT FORM
-class Filter_Table(npyscreen.Form):
-    def afterEditing(self):
-        self.parentApp.setNextForm("MAIN")
-
-    def create(self):
-        self.MSG = self.add(npyscreen.TitleText, name = "FILTER")
-
-class Nat_Table(npyscreen.Form):
-    def afterEditing(self):
-        self.parentApp.setNextForm("MAIN")
- 
-    def create(self):
-        self.MSG = self.add(npyscreen.TitleText, name = "NAT")
-
-class Mangle_Table(npyscreen.Form):
-    def afterEditing(self):
-        self.parentApp.setNextForm("MAIN")
- 
-    def create(self):
-        self.MSG = self.add(npyscreen.TitleText, name = "MANGLE")
-
-class Raw_Table(npyscreen.Form):
-    def afterEditing(self):
-        self.parentApp.setNextForm("MAIN")
- 
-    def create(self):
-        self.MSG = self.add(npyscreen.TitleText, name = "RAW")
-
-class Selinux_Table(npyscreen.Form):
-    def afterEditing(self):
-        self.parentApp.setNextForm("MAIN")
- 
-    def create(self):
-        self.MSG = self.add(npyscreen.TitleText, name = "SELINUX")
+        self.Table_Choice = self.add(TableList, max_height = 15, name = "Select a table", scroll_exit = True)
 
 
 class iptable_Editor(npyscreen.NPSAppManaged):
     def onStart(self):
         self.addForm('MAIN', table_Select_Form, name ='Select Table')
-        self.addForm('FILTER', Filter_Table, name ='FILTER Chain')
-        self.addForm('NAT', Nat_Table, name ='NAT Chain')
-        self.addForm('MANGLE', Mangle_Table, name ='MANGLE Chain')
-        self.addForm('RAW', Raw_Table, name ='RAW Chain')
-        self.addForm('SELINUX', Selinux_Table, name ='SELINUX Chain')
-
 if __name__ == '__main__':
     App = iptable_Editor().run()
