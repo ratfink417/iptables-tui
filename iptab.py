011519154bbae3c6a0680ae22c7a59e54f375c0d
ratfink417 <ratfink417@gmail.com>
2021-04-20 21:28:53 -0700

    menu header now changes

--- iptab.py
+++ iptab.py
@@ -5,29 +5,25 @@ import iptc
 class OptionsList(npyscreen.MultiLineAction):
     def __init__(self, *args, **keywords):
         super(OptionsList, self).__init__(*args, **keywords)
-        self.values = ['filter','nat','mangle','raw','selinux']
+        self.values = ['--- Select A Table ---','filter','nat','mangle','raw','selinux']
         self.tables = self.values
 
     def actionHighlighted(self, act_on_this, key_press):
         if self.values == self.tables:
-            self.parent.Menu_Heading.name = "--- Select Chain ---"
             selected_table = self.values[self.cursor_line]
-            self.values=[]
+            self.values=['--- Select A Chain ---']
             chains = iptc.easy.dump_table(selected_table, ipv6=False)
 
             for key in chains: self.values.append(key)
-            self.parent.Menu_Heading.display()
             self.display()
 
         else:
             self.values = self.tables
-            self.parent.Menu_Heading.name = "--- Select Table ---"
-            self.parent.Menu_Heading.display()
+            self.name = "--- Select Table ---"
             self.display()
 
 class table_Select_Form(npyscreen.Form):
     def create(self):
-        self.Menu_Heading = self.add(npyscreen.TitleFixedText, name = "--- Select Table ---" )
         self.Table_Choice = self.add(OptionsList, max_height = 15,scroll_exit = True)
 
 class iptable_Editor(npyscreen.NPSAppManaged):
